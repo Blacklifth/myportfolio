088c705725b87e16c4d5dac8f053226db2526be0
Blacklifth <marcelo.blc.syb@gmail.com>
2024-12-12 15:29:31 -0300

    added my info to page, added statistics component, added react count librarie, added statsData.

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,39 @@
 import Container from "@/components/Container";
+import SocialLinks from "@/components/SocialLinks";
 import { Button } from "@/components/ui/button";
+import { Download } from "lucide-react";
 export default function Home() {
     return (
         <div className="bg-bodyColor text-white/80">
-            <Container>
-                <h1>my portfolio nextjs</h1>
-                <Button>click me</Button>
+            <Container className="py-10 grid grid-cols-1 md:grid-cols-2 gp-10">
+                <div className="flex flex-col items-center md:items-start gap-5 md:gap-7 text-center md:text-start">
+                    <div>
+                        <h3 className="font-semibold tracking-wider mb-1">
+                            Computer Civil Engineer
+                        </h3>
+                        <h2 className="text-3xl md:text-5xl mb-2 text-white">
+                            Hello I&apos;m
+                        </h2>
+                        <h1 className="text-lightSky text-5xl md:text-7xl tracking-normal ">
+                            Marcelo Loyola
+                        </h1>
+                    </div>
+                    <p className="w-auto md:max-w[500px] text-white/50 font-thin leading-6 ">
+                        Civil Engineer in Computer Science with experience in
+                        technical support, web development, and digital platform
+                        management. Known for proactivity, analytical skills,
+                        and a continuous improvement mindset. I aim to join as a
+                        trainee, junior, or in a related role that allows me to
+                        apply my knowledge and grow professionally in a
+                        collaborative and challenging environment.
+                    </p>
+                    <Button className="bg-transparent rounded-full border border-lightSky/50 text-lightSky hover:bg-hoverColor hover:text-black hoverEffect h-11">
+                        Download cv <Download />{" "}
+                    </Button>
+                    <SocialLinks />
+                </div>
+                {/*Photo*/}
+                <div>Photo</div>
             </Container>
         </div>
     );
--- constants/index.ts
+++ constants/index.ts
@@ -4,3 +4,10 @@ export const navbarData = [
     { title: "Resume", href: "/resume" },
     { title: "Contact", href: "/contact" },
 ];
+
+export const statsData = [
+    { num: 1, title: "Years of experience" },
+    { num: 5, title: "Projects Completed" },
+    { num: 8, title: "Skills Mastered" },
+    { num: 200, title: "Code commits" },
+];
