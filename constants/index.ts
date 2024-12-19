fad51836e00e1ceb1fbe13b88ef813ba0d250a3e
Blacklifth <marcelo.blc.syb@gmail.com>
2024-12-19 13:29:23 -0300

    Services Page Added. -global css: created new class text-outline. -layout.tsx added new class -page.tsx added services cards  -PageLayouttsx added motion to ease in content -title.tsx added title ass children -index.tsx added new constants for servicesData

--- constants/index.ts
+++ constants/index.ts
@@ -11,3 +11,48 @@ export const statsData = [
     { num: 8, title: "Skills Mastered" },
     { num: 200, title: "Code commits" },
 ];
+
+export const servicesData = [
+    {
+        _id: "01",
+        title: "Web Development",
+        description:
+            "I specialize in creating modern, responsive websites that are optimized for SEO and designed to enhance user experience. Whether you need a landing page, an e-commerce platform, or a custom web application with secure authentication, I deliver high-performance solutions tailored to your business goals.",
+        href: "https://myportfolio-tau-hazel-90.vercel.app/",
+    },
+    {
+        _id: "02",
+        title: "Process Optimization and Documentation",
+        description:
+            "As a BPM consultant, I use tools like Bizagi to analyze, model, and optimize business workflows, ensuring maximum efficiency. My expertise includes creating use cases, process diagrams, and professional documentation to streamline operations and improve organizational productivity.",
+        href: "",
+    },
+    {
+        _id: "03",
+        title: "Maintenance and Technical Support",
+        description:
+            "I provide reliable IT maintenance and technical support to small businesses and educational institutions. My services include troubleshooting software and hardware issues, configuring systems, and ensuring the continuity of your IT infrastructure.",
+        href: "",
+    },
+    {
+        _id: "04",
+        title: "Task Automation",
+        description:
+            "Streamline your operations with custom automation solutions. I develop scripts using Bash and Python to automate repetitive tasks, optimize workflows, and improve overall productivity for your business.",
+        href: "",
+    },
+    {
+        _id: "05",
+        title: "Digital Marketing and SEO",
+        description:
+            "Increase your online visibility with websites that are optimized for search engines and tailored to your audience. I focus on responsive design, keyword optimization, and user-friendly navigation to help you rank higher and attract more traffic.",
+        href: "",
+    },
+    {
+        _id: "06",
+        title: "Professional Portfolio Creation",
+        description:
+            "Stand out with a custom-built portfolio website. I design polished, professional websites with integrated contact forms, WhatsApp, and hosting management. Perfect for showcasing your work and making a strong first impression.",
+        href: "",
+    },
+];
